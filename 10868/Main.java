9ca791523ce06717141f71b73f1211ab767bb61b
RumbleKAT <reki318@naver.com>
2019-08-02 21:26:22 +0900

    10868 modified

--- 10868/Main.java
+++ 10868/Main.java
@@ -1,53 +1,67 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main{
+public class Main {
+	
+	static long [] arr = new long [1000001];
+	static int N,M;
+	
+	public static void main(String [] args) throws Exception {
+		System.setIn(new FileInputStream("./sample.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(token.nextToken());
+		M = Integer.parseInt(token.nextToken());
+		
+		for(int i = 1;i<=N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		SegmentTree st = new SegmentTree(N,arr);
 
-    static int N,M;
-    static long [] arr;
-    static long [] seg;
+		long result = 0;
+	
+		for(int i = 0;i<M;i++) {
+			token = new StringTokenizer(br.readLine());
+			int left = Integer.parseInt(token.nextToken());
+			int right = Integer.parseInt(token.nextToken());
+		
+			result = st.get(1,1,N,left, right);
+			
+			System.out.println(result);
+		}
+		
+	}
+}
 
-    public static void main(String [] args) throws Exception{
-        System.setIn(new FileInputStream("./sample.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(token.nextToken());
-        M = Integer.parseInt(token.nextToken());
-
-        arr = new long [N+1];
-        seg = new long [N*4];
-
-        for(int i = 1;i<=N;i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            update(i,arr[i],1,1,N);
-        }
-
-        for(int i =1 ;i<=M;i++){
-            token = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(token.nextToken());
-            int b = Integer.parseInt(token.nextToken());
-
-            System.out.println(get(1,1,N,a,b));
-        }
-
-        //System.out.println(Arrays.toString(seg));
-
-    }
-
-    static long get(int node, int start, int end, int left, int right){
-        if(start > right || end < left) return Integer.MAX_VALUE;//해당 범위를 넘어서는 것은 최대값을 추출
-        if(left <= start && end <= right) return seg[node];
-        int mid = (start+end)/2;
-        return Math.min(get(node*2,start,mid, left, right),get(node*2+1,mid+1,end, left, right));
-    }
-
-    static long update(int index, long val, int node, int left, int right){
-        if(index < left || index > right) return seg[node];
-        if(left == right){
-            return seg[node] = val;
-        }
-        int mid = (left+right)/2;
-        return seg[node] = Math.min(update(index, val, node*2, left, mid), update(index, val, node*2+1, mid+1, right));
-    }
+class SegmentTree{
+	static int size;
+	static long [] arr;
+	static long [] args;
+	
+	public SegmentTree(int size, long[] arr2) {
+        this.size = size;
+		arr = new long[this.size*4];
+		this.args = arr2;
+		init(1,1,size);
+	}
+	
+	long init(int index, int start, int end) {
+		if(start == end) {
+			return arr[index] = args[start];
+		}
+		return arr[index] = Math.min(init(index*2,start,(start+end)/2) , init(index*2+1,(start+end)/2+1, end));		
+	}
+	
+	public long get(int index, int start, int end, int left, int right) {
+		if(start > right || end < left) {
+			return Integer.MAX_VALUE;
+		}
+		if(left <= start && end <= right) {
+			return arr[index];
+		}
+		return Math.min(get(index*2,start, (start+end)/2, left, right), get(index*2+1,(start+end)/2+1,end,left,right));
+	}
 }
