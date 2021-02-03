6c6ba6616abaffa9658e8e0a873ae32392bfc809
RumbleKAT <reki318@naver.com>
2021-02-03 21:42:56 +0900

    modify priorityqueue O(nlogn)

--- jsAlgorithm/PriorityQueue.js
+++ jsAlgorithm/PriorityQueue.js
@@ -1,3 +1,4 @@
+/*
 export default class PriorityQueue{
     constructor(){
         this.store = [];
@@ -20,4 +21,94 @@ export default class PriorityQueue{
         });
         return this.store.splice(entry,1);
     }
+}//https://www.geeksforgeeks.org/implementation-priority-queue-javascript/
+*/
+class Node{
+    constructor(value, priority){
+        this.value = value;
+        this.priority = priority;
+    }
 }
+
+class PriorityQueue{
+    constructor(){
+        this.values = [];
+    }
+
+    isEmpty(){
+        return this.values.length === 0 ? true : false;
+    }
+
+    swap(index1, index2){
+        let temp = this.values[index1];
+        this.values[index1] = this.values[index2];
+        this.values[index2] = temp;
+        return this.values;
+    }
+
+    bubbleUp(){
+        let index = this.values.length - 1;
+        while(index > 0){
+            let parentIdx = Math.floor((index-1)/2);
+            if(this.values[parentIdx].priority > this.values[index].priority){
+                this.swap(index, parentIdx);
+                index = parentIdx;
+            }else{
+                break;
+            }
+        }
+        return 0;
+    }
+
+    enqueue(value){
+        this.values.push(value);
+        this.bubbleUp();
+        return this.values;
+    }
+
+    bubbleDown(){
+        let parentIndex = 0;
+        const length = this.values.length;
+        const elementPriority = this.values[0].priority;
+
+        while(true){
+            let leftChildIndex = (2*parentIndex) +1;
+            let rightChildIndex = (2*parentIndex) + 2;
+            let leftChildPriority, rightChildPriority;
+            let indexToSwap = null;
+            
+            if(leftChildIndex < length){
+                leftChildPriority = this.values[leftChildIndex].priority;
+                if(leftChildPriority < elementPriority){
+                    indexToSwap = leftChildIndex;
+                }
+            }
+
+            if(rightChildIndex < length){
+                rightChildPriority = this.values[rightChildIndex].priority;
+                if((rightChildPriority < elementPriority && indexToSwap === null) || 
+                   (rightChildPriority < leftChildPriority && indexToSwap !== null)){
+                       indexToSwap = rightChildIndex;
+                   }
+            }
+
+            if(indexToSwap === null){
+                break;
+            }
+            this.swap(parentIndex, indexToSwap);
+            parentIndex = indexToSwap;
+        }
+    }
+
+    dequeue(){
+        this.swap(0,this.values.length-1);
+        let poppedNode = this.values.pop();
+        if(this.values.length > 1){
+            this.bubbleDown();
+        }
+        return poppedNode;
+    }
+
+
+}
+module.exports = PriorityQueue
--- jsAlgorithm/app.js
+++ jsAlgorithm/app.js
@@ -1,7 +1,25 @@
-var queue = require('./Queue');
+var PriorityQueue = require('./PriorityQueue');
+
+class Node{
+    constructor(value, priority){
+        this.value = value;
+        this.priority = priority;
+    }
+}
 
 
-var arr = new Array(3);
-for(var i=1;i<=3;i++){
-    arr[i] = new Array(3);
+
+var current = new Node(1,1);
+console.log(PriorityQueue);
+
+var pq = new PriorityQueue();
+
+pq.enqueue(new Node(1,1));
+pq.enqueue(new Node(5,2));
+pq.enqueue(new Node(3,0));
+
+
+while(!pq.isEmpty()){
+    let current = pq.dequeue();
+    console.log(current);
 }
