44c36cc7b3c2915adc4ad1fb7fe5d9c74453928c
RumbleKAT <reki318@naver.com>
2025-01-05 17:25:48 +0900

    feat: permutation

--- inflearn/dfs/Solution.java
+++ inflearn/dfs/Solution.java
@@ -1,57 +1,55 @@
 import java.util.*;
 class Solution {
-
-    static int [][] map;
+    static int [] arr;
     static boolean [] visited;
-    static int cnt;
-    static int [] result;
-    static Stack<Integer> pm;
+    static int slevel;
+    static int size;
+    static int [][] cCans;
+    static int min;
 
-	public int solution(int[][] fight){
+	public int solution(int[][] cans){
 		int answer = 0;
-        map = new int[8][8];
-        visited = new boolean[8];
-        result = new int[8];
-        pm = new Stack<>();
-        cnt = 0;
-        for(int i=0;i<fight.length;i++){
-            int x = fight[i][0];
-            int y = fight[i][1];
-            map[x][y] = 1;
-            map[y][x] = 1;
-        }
+        min = Integer.MAX_VALUE;
+        cCans = cans;
+        arr = new int[cans.length+1];
+        visited = new boolean [cans.length+1];
+		slevel = cans.length/2+1;
+        size = cans.length;
 
-        dfs(1,1);
-        answer = cnt;
+        dfs(0, 1);
+        answer = min;
+
 		return answer;
 	}
-
-    public static void dfs(int idx, int depth){
-        if(depth == 8){
-            cnt += 1;
-            return; 
+    public void dfs(int idx, int level){
+        if(level == size+1){
+            int wSum = 0;
+            int bSum = 0;
+            // pick up the 3 cans
+            for(int i=1;i<=cCans.length/2;i++){
+                wSum += cCans[arr[i]][0];
+            }
+            for(int i=cCans.length/2+1;i<=cCans.length;i++){
+                bSum += cCans[arr[i]][1];
+            }
+            min= Math.min(Math.abs(wSum-bSum),min);
+            return;
         }
-        for(int i=1;i<=7;i++){
-            if(!pm.isEmpty() && map[pm.peek()][i] == 1) continue;
+
+        for(int i=0;i<size;i++){
             if(!visited[i]){
                 visited[i] = true;
-                result[depth] = i;
-                pm.push(i);
-                dfs(i, depth +1);
+                arr[level] = i;
+                dfs(i+1,level+1);
                 visited[i] = false;
-                pm.pop();
             }
         }
-
     }
-
-		
+	
 	public static void main(String[] args){
 		Solution T = new Solution();
-		System.out.println(T.solution(new int[][]{{1, 3}, {5, 7}, {4, 2}}));
-		System.out.println(T.solution(new int[][]{{3, 2}, {3, 5}, {5, 2}, {7, 3}}));
-		System.out.println(T.solution(new int[][]{{1, 2}, {1, 5}, {1, 7}, {1, 3}}));
-		System.out.println(T.solution(new int[][]{{1, 7}}));
-		System.out.println(T.solution(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}));
+		System.out.println(T.solution(new int[][]{{87, 84}, {66, 78}, {94, 94}, {93, 87}, {72, 92}, {78, 63}}));
+		System.out.println(T.solution(new int[][]{{10, 20}, {15, 25}, {35, 23}, {55, 20}}));
+		System.out.println(T.solution(new int[][]{{11, 27}, {16, 21}, {35, 21}, {52, 21}, {25, 33},{25, 32}, {37, 59}, {33, 47}}));
 	}
 }
