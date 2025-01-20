f52246c5bc4fb601182083025649d869eaa8d813
RumbleKAT <reki318@naver.com>
2025-01-20 21:25:27 +0900

    feat: deque dfs

--- inflearn/dfs/Solution.java
+++ inflearn/dfs/Solution.java
@@ -1,63 +1,66 @@
 import java.util.*;
 
 class Solution {
-    static int [] arr;
-    static boolean [] visited;
-    static int slevel;
-    static int size;
-    static int [][] cCans;
-    static int min;
+    static Deque<Character> tmp;
+    static ArrayList<String> res;
+    static HashMap<Character, Integer> sH;
+    int len;
 
-	public int solution(int[][] cans){
-		int answer = 0;
-        min = Integer.MAX_VALUE;
-        cCans = cans;
-        arr = new int[cans.length+1];
-        visited = new boolean [cans.length+1];
-		slevel = cans.length/2+1;
-        size = cans.length;
-
-        dfs(0, 1);
-        answer = min;
-
-		return answer;
-	}
-    public void dfs(int idx, int level){
-        if(level == slevel){
-            int wSum = 0;
-            int bSum = 0;
-            ArrayList<Integer> A = new ArrayList<>();
-            ArrayList<Integer> B = new ArrayList<>();
-            for(int i=0;i<size;i++){
-                if(visited[i]){
-                    A.add(i);
-                }else{
-                    B.add(i);
-                }
-            }
-            // pick up the 3 cans
-            for(int i=0;i<cCans.length/2;i++){
-                wSum += cCans[A.get(i)][0];
-                bSum += cCans[B.get(i)][1];
-            }
-            min= Math.min(Math.abs(wSum-bSum),min);
+    public void dfs(){
+        if(tmp.size() == len){
+            String ts = "";
+            for(char x: tmp) ts += x;
+            res.add(ts);
             return;
         }
-
-        for(int i=0;i<size;i++){
-            if(!visited[i]){
-                visited[i] = true;
-                arr[level] = i;
-                dfs(i+1,level+1);
-                visited[i] = false;
+        else{
+            for(char key: sH.keySet()){
+                if(sH.get(key) == 0) continue;
+                tmp.addFirst(key);
+                tmp.addLast(key);
+                sH.put(key, sH.get(key)-2);
+                dfs();
+                tmp.pollFirst();
+                tmp.pollLast();
+                sH.put(key, sH.get(key)+2);
             }
         }
     }
-	
+
+
+	public String[] solution(String s){
+        tmp = new LinkedList<>();
+        res = new ArrayList<>();
+        sH = new HashMap<>();
+        len = s.length();
+        for(char x: s.toCharArray()){
+            sH.put(x, sH.getOrDefault(x, 0)+1);
+        }
+        int odd = 0;
+        char mid = '#';
+        for(char key: sH.keySet()){
+            if(sH.get(key) % 2 == 1){
+                mid = key;
+                odd++;
+            }
+        }
+        if(odd>1) return new String[]{};
+        if(mid != '#'){
+            tmp.add(mid);
+            sH.put(mid, sH.get(mid)-1);
+        }
+        dfs();
+        String [] answer = new String[res.size()];
+        for(int i=0;i<res.size();i++) answer[i] = res.get(i);
+        return answer;
+    }
+
 	public static void main(String[] args){
 		Solution T = new Solution();
-		System.out.println(T.solution(new int[][]{{87, 84}, {66, 78}, {94, 94}, {93, 87}, {72, 92}, {78, 63}}));
-		System.out.println(T.solution(new int[][]{{10, 20}, {15, 25}, {35, 23}, {55, 20}}));
-		System.out.println(T.solution(new int[][]{{11, 27}, {16, 21}, {35, 21}, {52, 21}, {25, 33},{25, 32}, {37, 59}, {33, 47}}));
+		System.out.println(Arrays.toString(T.solution("aaaabb")));	
+		System.out.println(Arrays.toString(T.solution("abbcc")));
+		System.out.println(Arrays.toString(T.solution("abbccee")));
+		System.out.println(Arrays.toString(T.solution("abbcceee")));
+		System.out.println(Arrays.toString(T.solution("ffeffaae")));
 	}
 }
